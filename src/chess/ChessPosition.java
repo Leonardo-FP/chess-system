a417801eb3c06f2105fd829871c7b9efdc1b5f7a
Leonardo-FP <leopaulellI@gmail.com>
2022-02-03 15:20:50 -0300

    Little improvement in board printing

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -7,7 +7,7 @@ public class ChessPosition {
 	private int row;
 	
 	public ChessPosition(char column, int row) {
-		if(column < 'a' || column < 'h' || row < 1 || row > 8) {
+		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8");
 		}
 		this.column = column;
